0cfb18f7568eb613269f88c935210351cc15524c
benson <showoowohs@gmail.com>
2017-01-12 16:34:30 +0800

    the flash_L511.sh scrpit is support only flash boot.img

--- utp_com/flash_L511.sh
+++ utp_com/flash_L511.sh
@@ -1,7 +1,50 @@
 #!/bin/bash
 # for imx6 android 5.1.1
 
+check_parameter(){
+  #echo "call check_parameter()"
+  if [[ $input_num -gt 3 ]];
+  then
+	  echo "comman error! please use ./flash_L511.sh help"
+	  exit 1
+  fi
+
+  if [ "x$parameter1" = "xboot" -o "x$parameter1" = "xbootimage" ]; then
+	  CMD="boot"
+  elif [ "x$parameter1" = "xh" -o "x$parameter1" = "xhelp" ]; then
+	  echo "usage: ./flash_L511.sh [boot] [system]"
+  fi
+}
+
+only_flash_boot(){
+  echo "only flash boot.img"
+  cd $UTP_COM_PATH
+  echo "enable boot partion 1 to boot"
+  ./utp_com -d $DEVICE -c "$ mmc bootpart enable 1 1 /dev/mmcblk3"
+
+  # Sending kernel uImage
+  echo "Sending kernel uImage"
+  ./utp_com -d $DEVICE -c "send" -f ${FLASH_IMAGE_DIR}/boot-imx6q.img
+  # write boot.img
+  echo "write boot.img"
+  ./utp_com -d $DEVICE -c "$ dd if=\$FILE of=/dev/mmcblk3p1"
+
+  echo "Finishing write"
+  ./utp_com -d $DEVICE -c "frf"
+
+  # Done
+  echo "Done"
+  ./utp_com -d $DEVICE -c "$ echo Update Complete!"
+  exit 1
+}
+
+
+input_num=$#
+parameter1=$1
+CMD=
 DEVICE=
+
+check_parameter
 
 IMX_USB_PATH=/home/benson/imx6_download_tool/imx_usb_loader
 UTP_COM_PATH=/home/benson/imx6_download_tool/utp_com/
@@ -52,6 +95,10 @@ lsusb | grep "15a2\:0054" && PID=1
 
 if [ ! "x$PID" = "x" ]; then
   exit
+fi
+
+if [ "x$CMD" = "xboot" ]; then
+	only_flash_boot
 fi
 
 
